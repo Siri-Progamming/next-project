6e25710608f07c28cde987c20d5a9b2b863cc55b
Sirika Hilaire <sirika.hilaire@ynov.com>
2024-03-23 14:57:37 +0100

    affichage du menu dropdown du header ok + animation smooth pour les éléments du header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,11 @@ import {useRouter} from "next/router";
 import SearchBar from "./Forms/SearchBar";
 import React, {useState} from "react";
 import IcoDropdown from "./Utils/IcoDropdown";
-import {useAuth} from "../contexts/AuthContext";
-import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
+
 
 const Header: React.FC = () => {
     const router = useRouter();
     const [showHeader, setShowHeader] = useState(false);
-    const {user} = useAuth();
     const handleClick = () => {
         router.push('/');
     };
@@ -31,23 +29,19 @@ const Header: React.FC = () => {
                         onMouseOver={handleMouseEnter}
                         className={`absolute inset-0 z-[999] w-full h-[10vh]`}></div>
                     <header onMouseLeave={handleMouseLeave}
-                            className={`flex flex-row text-wrap transition-height duration-700 overflow-hidden ${
-                                showHeader ? 'h-[var(--nav-height)]' : 'h-0'
+                            className={`flex flex-row text-wrap transition-height duration-700 ${
+                                showHeader ? 'h-[var(--nav-height)] showHeader' : 'h-0'
                             }`}>
                         <img id="logo" src="/fennec.png" alt="logo" onClick={handleClick} className="cursor-pointer"/>
-                        <div id={"title_sloggan"} onClick={handleClick} className="cursor-pointer">
+                        <div id="title_sloggan" onClick={handleClick} className="cursor-pointer">
                             <h1 className="">Fennext</h1>
                             <h2>What'ch next ?</h2>
                         </div>
-                        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                        <div id="movie-search-bar" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                             <SearchBar/>
                         </div>
-                        <div className="self-center absolute top-1/2 right-2 transform -translate-y-1/2">
-                            {user ? <IcoDropdown/> : <div className="avatar placeholder btn btn-lg btn-ghost btn-circle">
-                                <div className="bg-neutral text-neutral-content rounded-full">
-                                    <span className="text-2xl"><AccountCircleTwoToneIcon className="text-[80px] text-white opacity-93" /></span>
-                                </div>
-                            </div>}
+                        <div className="self-center absolute right-2">
+                            <IcoDropdown/>
                         </div>
                     </header>
                 </div>
--- src/components/Utils/IcoDropdown.tsx
+++ src/components/Utils/IcoDropdown.tsx
@@ -1,29 +1,36 @@
 import React from "react";
+import {useAuth} from "../../contexts/AuthContext";
+import AccountCircleTwoToneIcon from "@mui/icons-material/AccountCircleTwoTone";
+
 
 const IcoDropdown: React.FC = () => {
+    const {user} = useAuth();
     return (
         <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-lg  btn-circle btn-white avatar">
-                <div className="w-32 rounded-full">
-                    <img alt="User Avatar"
-                         src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                    />
 
-                </div>
-            </div>
-            <ul tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[2500] p-1 shadow bg-base-100 rounded-box w-52">
+            {!user ?
+                <div tabIndex={0} role="button"  className="avatar placeholder btn btn-lg btn-ghost btn-circle">
+                    <div className="bg-neutral text-neutral-content rounded-full">
+                    <span className="text-2xl"><AccountCircleTwoToneIcon className="text-[80px] text-white opacity-93"/></span>
+                    </div>
+                </div> :
+                <div tabIndex={0} role="button" className="btn btn-lg  btn-circle btn-white avatar">
+                    <div className="w-32 rounded-full">
+                        <img alt="User Avatar" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                    </div>
+                </div>}
+
+            <ul tabIndex={1}
+                className="menu menu-sm dropdown-content mt-3 z-[2500] p-2 shadow bg-base-100 rounded-box w-52">
                 <li>
                     <a className="justify-between">
                         Profile
                         {/*<span className="badge">New</span>*/}
                     </a>
                 </li>
-                <li><a>Settings</a></li>
-                <li><a>Logout</a></li>
+                <li><a>Déconnexion</a></li>
             </ul>
         </div>
     );
 }
-
 export default IcoDropdown;
