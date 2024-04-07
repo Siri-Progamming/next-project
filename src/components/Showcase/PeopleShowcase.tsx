b2ac32662c7d020d429bd725f99149d1ab48e90f
Sirika Hilaire <sirika.hilaire@ynov.com>
2024-04-07 21:27:07 +0200

    mise à la même échelle des photos cast que similar movies

--- src/components/Showcase/PeopleShowcase.tsx
+++ src/components/Showcase/PeopleShowcase.tsx
@@ -29,8 +29,8 @@ const PeopleShowcase: React.FC<MoviesProps> = ({movie, nbToShow, title}) => {
             <ul className="flex overflow-x-auto min-h-fit space-x-4 pb-4">
                 {casts?.slice(0, nbToShow).map(cast => (
                     <li key={cast.id}
-                        className="flex-column relative min-w-[95px] max-w-[95px]">
-                        {cast.profile_path ? showImage(cast) : showNoImage("min-w-[95px] max-w-[95px]", "h-[130px]", "text-[75px]", "rounded-xl")}
+                        className="flex-column relative min-w-[110px] max-w-[110px]">
+                        {cast.profile_path ? showImage(cast) : showNoImage("min-w-[110px] max-w-[110px]", "h-[160px]", "text-[75px]", "rounded-xl")}
                         <div className="pt-2 leading-none">
                             <p className="text-sm font-semibold">{cast.name}</p>
                             <p className="font-light text-xs text-white text-opacity-50"><span
@@ -45,7 +45,7 @@ const PeopleShowcase: React.FC<MoviesProps> = ({movie, nbToShow, title}) => {
     function showImage(cast: Cast) {
         return (
             <div
-                className="h-[130px] rounded-xl cursor-pointer"
+                className="h-[160px] rounded-xl cursor-pointer"
                 style={{
                     backgroundImage: `url(${ConfigService.themoviedb.urls.image_view + "/w220_and_h330_face" + cast.profile_path})`,
                     backgroundSize: 'cover',
