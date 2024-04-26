7f46c16f768e11eca2dc0287a7405fd927d82e46
Sirika Hilaire <sirika.hilaire@ynov.com>
2024-04-27 00:35:02 +0200

    Réglage pb key sur les <Image> des séries. code : Merci ❤️

--- src/components/Showcase/HorizontalEpisodesShowcase.tsx
+++ src/components/Showcase/HorizontalEpisodesShowcase.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import {Season} from "../../interfaces/Serie";
 import Image from "next/image";
 import {ConfigService} from "../../services/IMDB.API/config.service";
-import NoImage, {showNoImage} from "../Skeleton/NoData/NoImage";
+import {showNoImage} from "../Skeleton/NoData/NoImage";
 
-interface HorizontalEpisodeShowcaseProps{
-    seasons:Array<Season>
+interface HorizontalEpisodeShowcaseProps {
+    seasons: Array<Season>
 }
+
 const HorizontalEpisodeShowcase: React.FC<HorizontalEpisodeShowcaseProps> = ({seasons}) => {
     const [activeSeason, setActiveSeason] = React.useState<number>(0);
 
-    const changeActiveSeason = (index:number) => {
-        if(index != activeSeason){
+    const changeActiveSeason = (index: number) => {
+        if (index != activeSeason) {
             setActiveSeason(index);
-        }else{
+        } else {
             setActiveSeason(-1);
         }
 
@@ -21,6 +22,7 @@ const HorizontalEpisodeShowcase: React.FC<HorizontalEpisodeShowcaseProps> = ({se
     return (
         <div className="relative mt-[3vh]">
             <div className="ml-[3vw] mr-[3vw] ">
+                {/*SAISONS*/}
                 <div className="overflow-x-auto">
                     <ul className="flex flex-row gap-x-4">
                         {sortSeasons(seasons).map((season, index) => (
@@ -30,20 +32,22 @@ const HorizontalEpisodeShowcase: React.FC<HorizontalEpisodeShowcaseProps> = ({se
                         ))}
                     </ul>
                 </div>
-                <div className="overflow-x-auto">
-                    <ul className="flex flex-row gap-x-4 mb-4 mt-4">
-
+                {/*EPISODES*/}
+                <div className="mt-4">
+                    <p>{formatDateFR(seasons[activeSeason].air_date)} | {seasons[activeSeason].episode_count} épisodes</p>
+                    <ul className="flex flex-row gap-x-4 pb-4 pt-4 overflow-x-auto">
                         {(activeSeason != -1 && seasons[activeSeason].episodes?.length == 0) && (seasons[activeSeason].air_date === null ? "Coming soon !" : "Coming soon => " + seasons[activeSeason].air_date)}
 
                         {(activeSeason != -1 && seasons[activeSeason].episodes?.length > 0) && seasons[activeSeason].episodes.map((episode, index) => (
-                            <li key={index} className="">
+                            <li key={index+"-"+activeSeason} className="">
                                 <div className="relative w-[300px] h-[180px]">
                                     {episode.still_path == null ?
                                         showNoImage("w-[300px]", "h-[180px]", "text-[150px]", "rounded-sm")
                                         :
                                         <Image
                                             src={ConfigService.themoviedb.urls.image_view + "/original" + episode.still_path}
-                                            alt={"Image of " + episode.name} fill={true} className="rounded-sm"/>}
+                                            alt={"Image of " + episode.name} fill={true} className="rounded-sm"
+                                            />}
                                 </div>
 
                                 <div className="pt-2 leading-none">
@@ -70,4 +74,61 @@ function sortSeasons(seasons: Season[]) {
             return a.season_number - b.season_number;
         }
     });
+}
+
+function formatDateFR(date: string | null) {
+    if (date === null) return "Coming soon !";
+    let theDate = new Date(date).toLocaleDateString('fr-FR');
+    let day = theDate.split("/")[0];
+    let month = theDate.split("/")[1];
+    let year = theDate.split("/")[2];
+    switch (month) {
+        case "1":
+        case "01":
+            month = "Janvier";
+            break;
+        case "2":
+        case "02":
+            month = "Février";
+            break;
+        case "3":
+        case "03":
+            month = "Mars";
+            break;
+        case "4":
+        case "04":
+            month = "Avril";
+            break;
+        case "5":
+        case "05":
+            month = "Mai";
+            break;
+        case "6":
+        case "06":
+            month = "Juin";
+            break;
+        case "7":
+        case "07":
+            month = "Juillet";
+            break;
+        case "8":
+        case "08":
+            month = "Août";
+            break;
+        case "9":
+        case "09":
+            month = "Septembre";
+            break;
+        case "10":
+            month = "Octobre";
+            break;
+        case "11":
+            month = "Novembre";
+            break;
+        case "12":
+            month = "Décembre";
+            break;
+    }
+    theDate = month + " " + year;
+    return theDate;
 }
